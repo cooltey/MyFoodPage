3fea11799e56918c19964fc6355f91bc6dbf0c83
cooltey <coolteygame@gmail.com>
2017-04-05 13:25:47 -0700

    Initial Commit

---
+++ MyFoodPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFoodPage
+//
+//  Created by Cooltey Feng on 05/04/2017.
+//  Copyright © 2017 18841. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
